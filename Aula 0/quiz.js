45ba41c36a5258451c190e76805ce4cc74223892
Guto Nicacio <12159552+GutoNicacio@users.noreply.github.com>
2023-01-19 13:25:17 -0300

    Update quiz.js
    exibindo as opções dentro do loop sempre que for perguntar

--- Aula 0/quiz.js
+++ Aula 0/quiz.js
@@ -6,16 +6,17 @@ let opcaoC = "7";
 let opcaoD = "15";
 
 //exibe as opções e pede digitação da resposta
-alert(pergunta + "\n" + opcaoA + "\n" + opcaoB + "\n" + opcaoC + "\n" + opcaoD);
-
+//alert(pergunta + "\n" + opcaoA + "\n" + opcaoB + "\n" + opcaoC + "\n" + opcaoD);
 while (true) {
+    alert(pergunta + "\n" + opcaoA + "\n" + opcaoB + "\n" + opcaoC + "\n" + opcaoD);
+
     let opcaoDigitada = prompt("Digitte a opção correta:");
     if (opcaoDigitada != opcaoA
         && opcaoDigitada != opcaoB
         && opcaoDigitada != opcaoC
         && opcaoDigitada != opcaoD) {
         alert("Resposta inválida");
-    } else if {
+    } else if (opcaoDigitada != opcaoB) {
         alert("Resposta incorreta");
     } else {
         alert("Resposta correta");
