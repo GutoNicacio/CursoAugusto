5435d33f70abd35ea202f247d761079b1466eb29
Guto Nicacio <12159552+GutoNicacio@users.noreply.github.com>
2023-01-19 19:18:03 -0300

    Update odometro.js
    usando while ou for de forma aleatória

--- Aula 1/odometro.js
+++ Aula 1/odometro.js
@@ -5,25 +5,53 @@ let milhar = 0;
 
 let i = 0;
 
-while (i < 1000) {
-    console.log("|" + milhar + "|" + centena + "|" + dezena + "|" + unidade + "|");
+let numero = Math.round(Math.abs(Math.random() * (2 - 1) + 1));
 
-    unidade += 1;
-
-    if (unidade == 10) {
-        dezena += 1;
-        unidade = 0;
+if (numero == 1) {
+    while (i < 10000) {
+        console.log("|" + milhar + "|" + centena + "|" + dezena + "|" + unidade + "|");
+    
+        unidade += 1;
+    
+        if (unidade == 10) {
+            dezena += 1;
+            unidade = 0;
+        }
+    
+        if (dezena == 10) {
+            centena += 1;
+            dezena = 0;
+        }
+    
+        if (centena == 10) {
+            milhar += 1;
+            centena = 0;
+        }
+    
+        i += 1;
     }
-
-    if (dezena == 10) {
-        centena += 1;
-        dezena = 0;
+    alert("Executado no WHILE!");
+} else {
+    for (i=0; i<10000; i++) {
+        console.log("|" + milhar + "|" + centena + "|" + dezena + "|" + unidade + "|");
+    
+        unidade += 1;
+    
+        if (unidade == 10) {
+            dezena += 1;
+            unidade = 0;
+        }
+    
+        if (dezena == 10) {
+            centena += 1;
+            dezena = 0;
+        }
+    
+        if (centena == 10) {
+            milhar += 1;
+            centena = 0;
+        }
+    
     }
-
-    if (centena == 10) {
-        milhar += 1;
-        centena = 0;
-    }
-
-    i += 1;
+    alert("Executado no FOR!");
 }
