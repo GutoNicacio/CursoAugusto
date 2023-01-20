15f216e0f1b2875c5999468fcd9b5a3e36ec3fca
Guto Nicacio <12159552+GutoNicacio@users.noreply.github.com>
2023-01-20 19:36:33 -0300

    Update array.js
    alguns exemplos de array

--- Aula 3/array.js
+++ Aula 3/array.js
@@ -1,10 +1,55 @@
-let notas1 = [7,10,9,5,3,6,7];
+let notas = [7,10,9,5,3,6,7];
 
-// alert(notas1[0]);
+// alert(notas[0]);
 
 let total = 0;
-for (i=0; i<notas1.length; i++) {
-    total = total + notas1[i];
+for (i=0; i<notas.length; i++) {
+    total = total + notas[i];
 }
 
 alert(total/6);
+
+// XXX
+
+let notas = [
+    ["Augusto", 3],
+    ["João", 9],
+    ["Lucas", 2],
+    ["Chico", 10],
+    ["Joana", 9],
+    ["Luciana", 8]
+];
+
+const NMIN = 7;
+
+for (i=0; i<notas.length; i++) {
+    if (notas[i][1] < NMIN) {
+        console.log("Reprovado: " + notas[i][0]);
+    } else {
+        console.log("Aprovado: " + notas[i][0]);
+    }
+}
+
+// XXX
+
+let lista1 = [
+    ["Matemática"],
+    ["Português"],
+    ["História"],
+    ["Geografia"]
+]
+
+let notas = [];
+
+const NMIN = 7;
+
+let nota = 0;
+
+while (nota != "sair") {
+    nota = prompt("Digite uma nota ou sair:");
+    if (nota != "sair") {
+        notas.push(parseFloat(nota));
+    }
+}
+
+console.log(notas);
