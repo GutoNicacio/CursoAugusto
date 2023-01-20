c7191c96984bfe91a2eefa1674e68c5b668048a2
Guto Nicacio <12159552+GutoNicacio@users.noreply.github.com>
2023-01-20 07:58:11 -0300

    Update jogo_matematico.js
    incrementando o código

--- Aula 0/jogo_matematico.js
+++ Aula 0/jogo_matematico.js
@@ -1,24 +1,92 @@
 let pontos = 0;
 let vidas = 0;
+let max = 10
 
 while (true) {
-    let numero1 = Math.floor(Math.abs(Math.random() * (100 - 0) + 0));
-    let numero2 = Math.floor(Math.abs(Math.random() * (100 - 0) + 0));
+    let numero1 = Math.floor(Math.abs(Math.random() * (max - 1) + 1));
+    let numero2 = Math.floor(Math.abs(Math.random() * (max - 1) + 1));
+    let operacao = Math.floor(Math.random() * (3 - 0) + 0);
 
-    let valorDigitado = prompt("JOGO MATEMÁTICO!\n"
-    + "Vidas: " + vidas + ", pontos: " + pontos + "!\n"
-    + "Quanto é " + numero1 + " + " + numero2 + "?");
-
-    if (valorDigitado == numero1 + numero2) {
-        pontos = pontos + 1;
-        vidas = vidas + 1;
-    } else {
-        if (vidas > 0) {
-            alert("Incorreto!");
-            vidas = vidas - 1;
+    if (operacao == 0) {
+        let valorDigitado = prompt("JOGO MATEMÁTICO!\n"
+        + "Vidas: " + vidas + ", pontos: " + pontos + ", dificuldade: " + max/10 + ".\n"
+        + "Quanto é " + numero1 + " + " + numero2 + "?");
+    
+        if (valorDigitado == numero1 + numero2) {
+            pontos = pontos + 5;
+            vidas = vidas + 1;
+            if (pontos % 20 == 0) {
+                max = max + 10;
+            }
         } else {
-            alert("Game Over" + "\nVoce fez " + pontos + " pontos!");
-            break;
+            if (vidas > 0) {
+                alert("Incorreto!");
+                vidas = vidas - 1;
+            } else {
+                alert("Game Over!" + "\nVocê fez " + pontos + " pontos!");
+                break;
+            }
+        }    
+    } else if (operacao == 1) {
+        let valorDigitado = prompt("JOGO MATEMÁTICO!\n"
+        + "Vidas: " + vidas + ", pontos: " + pontos + ", dificuldade: " + max/10 + ".\n"
+        + "Quanto é " + numero1 + " - " + numero2 + "?");
+    
+        if (valorDigitado == numero1 - numero2) {
+            pontos = pontos + 5;
+            vidas = vidas + 1;
+            if (pontos % 20 == 0) {
+                max = max + 10;
+            }
+        } else {
+            if (vidas > 0) {
+                alert("Incorreto!");
+                vidas = vidas - 1;
+            } else {
+                alert("Game Over!" + "\nVocê fez " + pontos + " pontos!");
+                break;
+            }
         }
+    
+    } else if (operacao == 2) {
+        let valorDigitado = prompt("JOGO MATEMÁTICO!\n"
+        + "Vidas: " + vidas + ", pontos: " + pontos + ", dificuldade: " + max/10 + ".\n"
+        + "Quanto é " + numero1 + " / " + numero2 + "?");
+    
+        if (valorDigitado == numero1 / numero2) {
+            pontos = pontos + 5;
+            vidas = vidas + 1;
+            if (pontos % 20 == 0) {
+                max = max + 10;
+            }
+        } else {
+            if (vidas > 0) {
+                alert("Incorreto!");
+                vidas = vidas - 1;
+            } else {
+                alert("Game Over!" + "\nVocê fez " + pontos + " pontos!");
+                break;
+            }
+        }            
+    } else {
+        let valorDigitado = prompt("JOGO MATEMÁTICO!\n"
+        + "Vidas: " + vidas + ", pontos: " + pontos + ", dificuldade: " + max/10 + ".\n"
+        + "Quanto é " + numero1 + " * " + numero2 + "?");
+    
+        if (valorDigitado == numero1 * numero2) {
+            pontos = pontos + 5;
+            vidas = vidas + 1;
+            if (pontos % 20 == 0) {
+                max = max + 10;
+            }
+        } else {
+            if (vidas > 0) {
+                alert("Incorreto!");
+                vidas = vidas - 1;
+            } else {
+                alert("Game Over!" + "\nVocê fez " + pontos + " pontos!");
+                break;
+            }
+        }    
     }
 }
